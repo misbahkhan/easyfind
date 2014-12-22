4abea1fa9114626f30a6e648ed4694df3d765997
Misbah Khan <mk.misbah.khan@gmail.com>
2014-12-21 21:59:38 -0800

    Added basic login and sign up methods.

--- easy find 2/ViewController.m
+++ easy find 2/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <Parse/Parse.h>
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *username;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+@property (strong, nonatomic) IBOutlet UILabel *error;
 
 @end
 
@@ -17,6 +21,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)login:(id)sender {
+    [PFUser logInWithUsernameInBackground:[_username text] password:[_password text]
+        block:^(PFUser *user, NSError *error) {
+            [self nextstep:error];
+        }];
+}
+
+- (IBAction)signup:(id)sender {
+    PFUser *user = [PFUser user];
+    user.username = [_username text];
+    user.password = [_password text];
+    user.email = [_username text];
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        [self nextstep:error];
+    }];
+}
+
+- (void) nextstep:(NSError *)error
+{
+    if (!error) {
+        [self performSegueWithIdentifier:@"in" sender:self];
+    } else {
+        [_error setText:[error userInfo][@"error"]];
+    }
 }
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField
